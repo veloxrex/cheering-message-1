c192198eda203789504330d7b485a831e2ea9e04
VN20K20054 <tri.tranminh@zoi.tech>
2026-04-13 16:07:16 +0700

    allow iframe embed for Canva

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,23 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *;",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
